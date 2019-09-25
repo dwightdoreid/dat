23a3fcb98e834884568cdb35c4477b8e87e4b426
Dwight Reid <dwightdoreid@users.noreply.github.com>
2019-09-25 05:06:53 -0500

    Add files via upload

--- drDAT/app.py
+++ drDAT/app.py
@@ -1,8 +1,13 @@
+
 #!/usr/bin/python
 import subprocess
 from flask import Flask, render_template, send_from_directory
-from analyses.test1 import test1
+from analyses.BasicDataFilter import BasicDataFilter
+from analyses.BasicStatistics import BasicStatistics
 from analyses.HelloWorld import HelloWorld
+from analyses.SysData import SysData
+from analyses.test1 import test1
+from analyses.test2 import test2
 
 app = Flask(__name__)
 
@@ -13,20 +18,32 @@ app = Flask(__name__)
 def hello_world():
    return render_template('home.html')
 
-@app.route('/img/<path:filename>') 
-def send_file(filename): 
-    return send_from_directory('.', filename)
+@app.route('/img/<path:filename>', methods = ['POST', 'GET']) 
+def send_file(filename):
+	return send_from_directory('.', filename)
 
 
 ##Analyses Functions
 #########################################################################################################
-@app.route('/test1')
-def analysis_test1():
-   return test1.test1()
 
+@app.route('/BasicDataFilter')
+def analysis_BasicDataFilter():
+	return BasicDataFilter.BasicDataFilter()
+@app.route('/BasicStatistics')
+def analysis_BasicStatistics():
+	return BasicStatistics.BasicStatistics()
 @app.route('/HelloWorld')
 def analysis_HelloWorld():
-   return HelloWorld.HelloWorld()
+	return HelloWorld.HelloWorld()
+@app.route('/SysData')
+def analysis_SysData():
+	return SysData.SysData()
+@app.route('/test1')
+def analysis_test1():
+	return test1.test1()
+@app.route('/test2')
+def analysis_test2():
+	return test2.test2()
 
 if __name__ == '__main__':
    app.run(debug = True)
--- drDAT/appInit.py
+++ drDAT/appInit.py
@@ -52,7 +52,7 @@ strng = strng + """
 if __name__ == '__main__':
    app.run(debug = True)
 """
-appFile = open("app2.py", "w+")
+appFile = open("app.py", "w+")
 appFile.write(strng)
 appFile.close()
 
@@ -71,7 +71,11 @@ strng2 = """
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 
     <style>
         .loader {
@@ -112,7 +116,10 @@ strng2 = """
     <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <h1>Data Analysis Tool</h1>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="http://localhost:5000">Data Analysis Tool</a>
+    </nav>
+    <div class="container-fluid">
     <!-- <a href="http://localhost:5000/julia">Run</a> -->
     <!-- <img src="http://localhost:5000/img/images/fig.png" alt="plot"> -->
     <form>
@@ -147,6 +154,7 @@ strng2 = strng2 + """
             xhttp.send();
         }
     </script>
+    </div>
 </body>
 
 </html>
